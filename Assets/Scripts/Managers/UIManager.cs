bda61a0f150ad752b457ad4893c9da7f68756ed9
Kim Jeoung Gi <kjk9982098@daum.net>
2025-02-05 17:05:59 +0900

    Update

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 전역에서 접근 가능한 UI 관리 시스템을 구현
+/// UIManager.Instacne를 통해 UI 조작
+/// </summary>
+public class UIManager : SingleTonMonoBehaviour<UIManager>
+{
+    /// <summary>
+    /// UI_HUD 동시에 한개밖에 켤 수 없음
+    /// UI_Popup은 동시에 여러개 킬 수 있지만, UI 계층 구조에 영향을 받아 보이지 않는 UI가 있을 수 있음
+    /// </summary>
+    private Dictionary<System.Type, UIBase> _uoDict = new Dictionary<System.Type, UIBase>();
+    private UIHUD _currentHUD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/UIHUD.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// HUD = Head-Up Display
+/// 게임안에서 플레이어가 직접 건드리지 못하는 게임의 UI
+/// 게임의 상태창이나 점수 같은 거
+/// </summary>
+public class UIHUD : UIBase
+{
+
+}
