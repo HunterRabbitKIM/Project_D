e633dfe8eb66542cd3e7e79dd7b37301fa514963
Kim Jeoung Gi <kjk9982098@daum.net>
2025-02-06 18:23:42 +0900

    Update

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -12,18 +12,21 @@ public class UIManager : SingleTonMonoBehaviour<UIManager>
     /// UI_HUD 동시에 한개밖에 켤 수 없음
     /// UI_Popup은 동시에 여러개 킬 수 있지만, UI 계층 구조에 영향을 받아 보이지 않는 UI가 있을 수 있음
     /// </summary>
-    private Dictionary<System.Type, UIBase> _uoDict = new Dictionary<System.Type, UIBase>();
+    private Dictionary<System.Type, UIBase> _uiDict = new Dictionary<System.Type, UIBase>();
     private UIHUD _currentHUD;
 
-    // Start is called before the first frame update
-    void Start()
+    public void RegisterUI(UIBase ui)
     {
-        
-    }
+        var type = ui.GetType();
+        if(!_uiDict.ContainsKey(type))
+        {
+            _uiDict.Add(type, ui);
+            ui.Deactive();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if(transform.childCount == _uiDict.Count)
+            {
+                
+            }
+        }
     }
 }
--- Assets/Scripts/UI/UIBase.cs
+++ Assets/Scripts/UI/UIBase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIBase : MonoBehaviour
@@ -7,12 +8,16 @@ public class UIBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //UIManager.Instance.RegisterUI(this);
+        UIManager.Instance.RegisterUI(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Active()
     {
-        
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Deactive()
+    {
+        gameObject.SetActive(false);
     }
 }
